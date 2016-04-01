ce3204c7dd53a0116ce3a91fd12919d606c306f5
Jon Leech <oddhack@sonic.net>
2016-04-01 03:04:38 -0700

    Change log for April 1, 2016 Vulkan 1.0.8 spec update:
      * Bump API patch number and header version number to 8 for this
        update.
    
    Github Issues:
    
      * Specify in the validity language for flink:vkBeginCommandBuffer that
        pname:commandBuffer mustnot: currently be pending execution (public
        issue 96).
      * Describe depth comparison using the correct temporary variable names
        in the <<textures-depth-compare-operation,Depth Compare Operation>>
        section (public issue 100).
      * Clarify the order of descriptor update operations in the
        flink:vkUpdateDescriptorSets command (public issue 115).
      * Specify in the VK_KHR_swapchain extension that
        flink:vkAcquireNextImageKHR's pname:semaphore and pname:fence
        parameters cannot both be sname:VK_NULL_HANDLE (partly addresses,
        but does not fully close, public issue 117 / internal issue 246).
      * Change reference to the "lifetime" of a Vulkan command to
        "duration", and define the "duration" term (public issue 135).
      * Added valid usage language for slink:VkImageLayout to require both
        pname:height and pname:depth to be 1 for 1D images and pname:depth
        to be 1 for 2D images (public issue 137).
      * Fix SPIR-V example code in the
        <<descriptorsets-inputattachment,Input Attachment>> section to
        properly decorate the code:InputAttachmentIndex (public issue 139).
      * Fix reference to nonexistent pname:imageInfo in the description of
        flink:VkWriteDescriptorSet to refer to pname:pImageInfo (public
        issue 140).
    
    Internal Issues:
    
      * Link to the fixed-function vertex chapter from the drawing chapter
        (internal issue #110)
      * Fix typo in slink:VkImageCreateInfo validity language:
        ptext:maxExtent.sampleCounts -> pname:sampleCounts (internal issue
        249).
      * Explain why the non-core token etext:VK_IMAGE_LAYOUT_PRESENT_SRC_KHR
        is used in the example in the
        <<synchronization-semaphores,Semaphores>> section (internal issue
        251).
      * Attempt to clarify in the VK_KHR_android_surface extension's
        <<platformQuerySupport_android,Android Platform Support>> section
        that there is no Android-specific WSI query, and why (internal issue
        252).
    
    Other Commits:
    
      * Add missing language about ename:VK_INCOMPLETE being returned from
        array queries when the passed array is too short, in the
        VK_KHR_display, VK_KHR_swapchain, and VK_KHR_surface extensions.

--- src/vulkan/vulkan.h
+++ src/vulkan/vulkan.h
@@ -50,7 +50,7 @@ extern "C" {
 #define VK_VERSION_MINOR(version) (((uint32_t)(version) >> 12) & 0x3ff)
 #define VK_VERSION_PATCH(version) ((uint32_t)(version) & 0xfff)
 // Version of this file
-#define VK_HEADER_VERSION 7
+#define VK_HEADER_VERSION 8
 
 
 #define VK_NULL_HANDLE 0
