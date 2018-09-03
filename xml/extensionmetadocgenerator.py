89c36bd6a81fd3251e5ad11f68e1c4a1ef4c938c
Petr Kraus <petr_kraus@email.cz>
2018-09-03 17:54:53 +0200

    Fix special characters in GitHub contacts links (fix PR #770)

--- xml/extensionmetadocgenerator.py
+++ xml/extensionmetadocgenerator.py
@@ -254,7 +254,7 @@ class Extension:
             if handle.startswith('gitlab:'):
                 prettyHandle = 'icon:gitlab[alt=GitLab, role="red"]' + handle.replace('gitlab:@', '')
             elif handle.startswith('@'):
-                trackerLink = 'https://github.com/KhronosGroup/Vulkan-Docs/issues/new?title=' + self.name + ':%20&body=' + handle + '%20'
+                trackerLink = 'link:++https://github.com/KhronosGroup/Vulkan-Docs/issues/new?title=' + self.name + ':%20&body=' + handle + '%20++'
                 prettyHandle = trackerLink + '[icon:github[alt=GitHub, role="black"]' + handle[1:] + ']'
             else:
                 prettyHandle = handle
