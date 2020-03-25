0fa8bf59b20e82f68321174ef1eb4fa0d5b98e68
Jon Leech (Jon Leech) <oddhack@sonic.net>
2020-03-25 09:21:55 +0000

    Generate symlinks from refpage aliases to the API they're aliasing.
    This works around the problem that aliases do not have their own
    refpages, since they're included in the refpage for the API they're
    aliasing.

---
+++ scripts/makemanaliases.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+#
+# Copyright (c) 2020 The Khronos Group Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Script to create symbolic links for aliases in reference pages
+   Usage: makemanaliases.py -refdir refpage-output-directory"""
+
+import argparse
+import os
+import sys
+import vkapi
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-refdir', action='store',
+                        required=True,
+                        help='Path to directory containing reference pages to symlink')
+
+    args = parser.parse_args()
+
+    # Change to refpage directory
+    try:
+        os.chdir(args.refdir)
+    except:
+        print('Cannot chdir to', args.refdir, file=sys.stderr)
+        sys.exit(1)
+
+    # For each alias in the vkapi alias map, create a symlink if it
+    # doesn't exist - and warn if it does exist.
+
+    for key in vkapi.alias:
+        alias = key + '.html'
+        src = vkapi.alias[key] + '.html'
+
+        if not os.access(src, os.R_OK):
+            # This shouldn't happen, but is possible if vkapi.py isn't
+            # generated for the same set of APIs as were the refpages.
+            print('No source file', src, file=sys.stderr)
+            continue
+
+        if os.access(alias, os.R_OK):
+            # If the link already exists, that's not necc. a problem, so
+            # don't fail, but it should be checked out
+            print('Unexpected alias file "' + alias + '" exists, skipping',
+                  file=sys.stderr)
+        else:
+            # Create link from alias refpage to page for what it's aliasing
+            os.symlink(src, alias)
