3eace5224481d2b2845c166f9ebb3ed2d0f6f6d2
Petr Kraus <petr_kraus@email.cz>
2020-05-31 20:42:09 +0200

    Move katex script to proper dir
    - move `translate_math.js` to `scripts`
    - cleanup in Makefile to complement it
    - add script file header

--- scripts/translate_math.js
+++ scripts/translate_math.js
@@ -1,9 +1,27 @@
-const katex = require("./katex/katex.js");
+/* Copyright (c) 2020 The Khronos Group Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// Translates the latexmath in html on build time using KaTeX
+// Usage: nodejs translate_math.js katex/katex.min.js vkspec.html
+
+const katex = require(process.argv[2]);
 const fs = require("fs");
 const escapeRegex = require("escape-string-regexp");
 const he = require('he');
 
-const filepath = process.argv[2];
+const filepath = process.argv[3];
 
 var html = fs.readFileSync(filepath, "utf8");
 
