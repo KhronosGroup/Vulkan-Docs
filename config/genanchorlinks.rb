aa657a6294e564a84b23b8254ab31e2e0579addc
Jon Leech <oddhack@sonic.net>
2023-04-27 02:46:48 -0700

    Change log for April 27, 2023 Vulkan 1.3.249 spec update:
    Github Issues:
    
      * Add a non-normative NOTE on variability in
        <<textures-texel-anisotropic-filtering, Texel Anisotropic Filtering>>
        implementations, and move the theoretical implementation described in
        that section into the NOTE (public issue 1361).
      * Fix XML attributes of slink:VkMicromapEXT structure members to allow
        `NULL` handles (public issue 2114).
    
    Internal Issues
    
      * Clarify common draw VUs for attachment read feedback loops (internal
        issue 3439).
      * Add flink:vkCmdCopyQueryPoolResults VU requiring queries to have been
        made available by prior executed commands (internal issue 3451).
      * Relax ptext:*DescriptorSize limits to 256 bytes max in the
        <<limits-required, Required Limits>> table (internal issue 3456).
      * Mention shader objects in definitions of code:SubgroupLocalInvocationId
        and code:SubgroupSize built-ins (internal issue 3459).
      * Use numeric format (e.g. etext:*SFLOAT), numeric type (e.g. "`float`"),
        and SPIR-V type (e.g. code:OpTypeFloat, width, sign) consistently in the
        specification, rather than mixing and matching by accident (internal
        merge request 5627).
      * Add <<spirvenv-module-validation-runtime,
        Runtime SPIR-V Validation>> VUs for Mesh and Task
        code:max*Size using the same wording as for
        code:maxComputeSharedMemorySize (internal merge request 5804).
      * Add missing `const` to
        slink:VkVideoDecodeH265PictureInfoKHR::pname:pStdPictureInfo (internal
        merge request 5859).
      * Convert `scripts/genanchorlinks.py` script into an asciidoctor
        extension.
    
    New Extensions
    
      * apiext:VK_KHR_ray_tracing_position_fetch

---
+++ config/genanchorlinks.rb
@@ -0,0 +1,23 @@
+# Copyright 2023 The Khronos Group, Inc.
+# SPDX-License-Identifier: Apache-2.0
+
+# Rewrite VUID anchors with 'href' attributes so they can be selected in a
+# browser.
+
+require 'asciidoctor/extensions' unless RUBY_ENGINE == 'opal'
+
+include ::Asciidoctor
+
+class AnchorLinkPostprocessor < Asciidoctor::Extensions::Postprocessor
+  def process document, output
+    content = (document.attr 'copyright') || 'Copyright Acme, Inc.'
+    if document.basebackend? 'html'
+      output = output.gsub(/<a id="(VUID\-[\w\-:]+)">/, '<a id="\1" href="#\1">')
+    end
+    output
+  end
+end
+
+Asciidoctor::Extensions.register do
+  postprocessor AnchorLinkPostprocessor
+end
